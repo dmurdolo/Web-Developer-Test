649843df9549d7d6e72495ea310e065c7abd2645
Daniel <dmurdolo@gmail.com>
2020-12-29 17:20:49 +0000

    Amend API setup to work with vercel

--- api/index.js
+++ api/index.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const cartController = require('./controllers/cart');
-const productController = require('./controllers/product');
+const data = require('../data/products/index.json');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// API endpoints
-app.get('/api/products', productController.products);
-app.post('/api/checkout', cartController.checkout);
+app.get('/api/products', (req, res) => {
+    // Add some quantities to simulate the complete process of having added items to the cart
+    data && data.items.forEach(function (el) {
+        el.quantity = 1;
+    });
+
+    res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+    return res.status(200).json({ ok: true, message: 'Got products', products: data && data.items.filter(el => el.stockLevel > 0) });
+});
 
 module.exports = app;
