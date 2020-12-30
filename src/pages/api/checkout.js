d5fc2561e06951dd6d5cffe49e0f895b17c6b0d7
Daniel <dmurdolo@gmail.com>
2020-12-30 10:57:03 +0000

    Move API files into nextjs route structure for Vercel deployment

---
+++ src/pages/api/checkout.js
@@ -0,0 +1,4 @@
+export default function handler(req, res) {    
+    const orderNum = Math.random().toString(36).substring(7);
+    return res.status(200).json({ ok: true, message: 'Checkout complete', orderId: orderNum });
+}
---
+++ src/pages/api/products.js
@@ -0,0 +1,11 @@
+const data = require('../../../data/products/index.json');
+
+export default function handler(req,res) {
+    // Add some quantities to simulate the complete process of having added items to the cart
+    data && data.items.forEach(function (el) {
+        el.quantity = 1;
+    });
+
+    res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+    return res.status(200).json({ ok: true, message: 'Got products', products: data && data.items.filter(el => el.stockLevel > 0) });
+}
