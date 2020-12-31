b7741915fd2add57459f38d165367881af84f5aa
Daniel <dmurdolo@gmail.com>
2020-12-31 11:53:45 +0000

    Update favicon path

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -12,17 +12,17 @@ class MyDocument extends Document {
                     <meta name='description' content='The craziest alpaca app in the world' />
                     <meta name='format-detection' content='telephone=no' />
                     <meta name='mobile-web-app-capable' content='yes' />
-                    <meta name='msapplication-config' content='/static/icons/browserconfig.xml' />
+                    <meta name='msapplication-config' content='/icons/browserconfig.xml' />
                     <meta name='msapplication-TileColor' content='#2B5797' />
                     <meta name='msapplication-tap-highlight' content='no' />
                     <meta name='theme-color' content='#000000' />
-                    
-                    <link rel='apple-touch-icon' sizes='180x180' href='/public/icons/apple-touch-icon.png' />
-                    <link rel='icon' type='image/png' sizes='32x32' href='/public/icons/favicon-32x32.png' />
-                    <link rel='icon' type='image/png' sizes='16x16' href='/public/icons/favicon-16x16.png' />
-                    <link rel='manifest' href='/public/manifest.json' />
-                    <link rel='mask-icon' href='/public/icons/safari-pinned-tab.svg' color='#5bbad5' />
-                    <link rel='shortcut icon' href='/public/icons/favicon.ico' />
+
+                    <link rel='apple-touch-icon' sizes='180x180' href='/icons/apple-touch-icon.png' />
+                    <link rel='icon' type='image/png' sizes='32x32' href='/icons/favicon-32x32.png' />
+                    <link rel='icon' type='image/png' sizes='16x16' href='/icons/favicon-16x16.png' />
+                    <link rel='manifest' href='/manifest.json' />
+                    <link rel='mask-icon' href='/icons/safari-pinned-tab.svg' color='#5bbad5' />
+                    <link rel='shortcut icon' href='/icons/favicon.ico' />
                 </Head>
                 <body>
                     <Main />
